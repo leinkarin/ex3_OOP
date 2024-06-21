7ab8adb08c0b79d0210257b63dc5a9e94f6f6348
ronenlevyy <ronen.levy@mail.huji.ac.il>
2024-06-21 18:33:43 +0300

    Add files via upload

---
+++ src/ascii_art/Shell.java
@@ -0,0 +1,52 @@
+package ascii_art;
+
+//todo- this code is right from the getting ready code!
+//todo- this code is right from the getting ready code!
+//todo- this code is right from the getting ready code!
+//todo- this code is right from the getting ready code!
+//todo- this code is right from the getting ready code!
+
+
+
+
+// in ex6 its:
+//import ascii_art.img_to_char.BrightnessImgCharMatcher;
+//now in the new folder its:
+import ascii_art.AsciiArtAlgorithm;
+//
+
+import ascii_output.HtmlAsciiOutput;
+import image.Image;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Shell {
+//    private Set<Character> charSet = new HashSet<>();
+//    private static final String CMD_EXIT = "exit";
+//    private final Image img;
+//
+//    public Shell(Image img) {
+//        this.img = img;
+//    }
+//
+//    public void run() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print(">>> ");
+//        String cmd = scanner.next().trim();
+//        while(!cmd.toLowerCase().equals(CMD_EXIT)) {
+//            var param = scanner.nextLine().trim();
+//            System.out.print(">>> ");
+//            cmd = scanner.next();
+//        }
+//        Character[] charSet = {'a', 'b', 'c', 'd'};
+//        BrightnessImgCharMatcher charMatcher = new BrightnessImgCharMatcher();
+//        char[][] chars = charMatcher.chooseChars(img,4, charSet);
+//        HtmlAsciiOutput asciiOutput = new HtmlAsciiOutput("output.html", "Ariel");
+//        asciiOutput.output(chars);
+//        System.out.println(Arrays.deepToString(chars));
+//    }
+//
+}
--- src/image_char_matching/SubImgCharMatcher.java
+++ src/image_char_matching/SubImgCharMatcher.java
@@ -6,20 +6,33 @@ import image.Image;
 
 import java.awt.*;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Set;
+
+//todo- if we add more public func we need to explain in the README file
 
 public class SubImgCharMatcher {
 
 
     char[] charSet;
+    private HashMap<Character, Double> brightnessMap;
 
+
+    /**
+     *  ///todo- might be wrong;
+     * @param charset
+     */
     public SubImgCharMatcher(char[] charset){
-
-        ///todo- might be wrong;
-        this.charSet = new char[charset.length];
-        for(int i = 0; i<charset.length; i++){
-            this.charSet[i] = charset[i];
+        this.charSet = charset;
+        this.brightnessMap = new HashMap<>();
+        for (char c : this.charSet){
+            this.brightnessMap.put(c, calculateBrightnessLevels(c));
         }
+//        linearStretch();
+
+
+
     }
 
 
@@ -30,13 +43,94 @@ public class SubImgCharMatcher {
 
     //todo- this func add c to the chrSet
     public void addChar(char c){
+        char[] newCharSet = new char[charSet.length+1];
+        System.arraycopy(charSet, 0, newCharSet, 0, charSet.length);
+        newCharSet[charSet.length] = c;
+        this.charSet = newCharSet;
 
     }
 
-    //todo- this func remove c from thecharSet
-    public void removeChar(char c){}
+    //todo- this func remove c from the charSet
+    public void removeChar(char c){
+        //todo- add an exception if the char is not found
+        char[] newCharSet = new char[charSet.length-1];
+        int index = 0;
+        for (char value : charSet) {
+            if (value == c) {
+                continue;
+            }
+            newCharSet[index] = value;
+            index++;
+        }
+        this.charSet = newCharSet;
+    }
 
-    //todo- if we add more public funcs we need to explain in the README file
+
+
+    /**
+     * step 1 + 2;
+     * @param theCar
+     * @return
+     */
+    private double calculateBrightnessLevels(char theCar) {
+        boolean[][] charAsMatrix = CharConverter.convertToBoolArray(theCar);
+        double whitePixels = 0;
+        for (boolean[] row : charAsMatrix) {
+            for (boolean pixel : row){
+                if (pixel) {
+                    whitePixels++;
+                }
+            }
+        }
+        return whitePixels / (charAsMatrix.length*charAsMatrix[0].length);
+    }
+
+
+    /**step 3
+     *
+     * @param
+     * @return
+     */
+    private void linearStretch() {
+        double minBrightness = Double.MAX_VALUE;
+        double maxBrightness = Double.MIN_VALUE;
+
+        for (char brightness : brightnessMap.keySet()) {
+            if (brightnessMap.get(brightness) < minBrightness) {
+                minBrightness = brightnessMap.get(brightness);
+            }
+            if (brightnessMap.get(brightness) > maxBrightness) {
+                maxBrightness = brightnessMap.get(brightness);
+            }
+        }
+        for (char key : brightnessMap.keySet()) {
+            double newBrightness = (brightnessMap.get(key) - minBrightness) / (maxBrightness - minBrightness);
+            brightnessMap.replace(key, newBrightness);
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        char[] charSet = {'m', 'o'};
+        char[] charSet2 = {'A', 'B', 'C', 'D'};
+        SubImgCharMatcher macher = new SubImgCharMatcher(charSet2);
+        for ( char key : macher.brightnessMap.keySet()){
+            System.out.println(macher.brightnessMap.get(key));
+        }
+        macher.linearStretch();
+        for ( char key : macher.brightnessMap.keySet()){
+            System.out.println(macher.brightnessMap.get(key));
+        }
+
+    }
+
+
+
+
+
 
 
 
